6fa0bf563c98bcd973190e6745f8938b1364efc9
MAX1MDEV <58599802+MAX1MDEV@users.noreply.github.com>
2024-08-11 02:54:36 +0500

    Now works fine

--- FreeruCompanion.user.js
+++ FreeruCompanion.user.js
@@ -2,7 +2,7 @@
 // @name Freeru Companion
 // @author MaximDev
 // @namespace MaximDev
-// @version 2.1
+// @version 3.0
 // @homepage https://github.com/MAX1MDEV/FreeruCompanion
 // @supportURL https://github.com/MAX1MDEV/FreeruCompanion/issues
 // @updateURL https://raw.githubusercontent.com/MAX1MDEV/FreeruCompanion/main/FreeruCompanion.user.js
@@ -62,7 +62,7 @@
 
   mainButton.addEventListener('click', function() {
     confirmTasks();
-    window.open('', '_self').close();
+    window.open('', '_blank').close();
   });
 
   mainButton.addEventListener('mouseover', function() {
@@ -90,37 +90,23 @@
   });
 
   function confirmTasks() {
-    var buttons = document.querySelectorAll('.task-card__button.task-card__button_stretch.btn.btn-blue');
-    buttons.forEach(function(button) {
-      button.addEventListener('click', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-      });
-      if (!button.hasAttribute('href') && !button.hasAttribute('target')) {
-        var event = new MouseEvent('click', {
-          bubbles: true,
-          cancelable: true,
-          view: window,
-          detail: 0,
-          buttons: 0,
-          clientX: 0,
-          clientY: 0,
-          ctrlKey: false,
-          altKey: false,
-          shiftKey: false,
-          metaKey: false,
-        });
-        button.dispatchEvent(event);
-      }
-    });
-
-    var buttons_con = document.querySelectorAll('.task-card__button.task-card__button_stretch.btn.btn-bordered');
-    buttons_con.forEach(function(button) {
-      button.addEventListener('click', function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-      });
+  var buttons = document.querySelectorAll('.task-card__button.task-card__button_stretch.btn.btn-blue');
+  buttons.forEach(function(button) {
+    if (button.tagName.toLowerCase() === 'button') {
       button.click();
-    });
-  }
+    } else {
+      var clickEvent = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+      });
+      button.dispatchEvent(clickEvent);
+    }
+  });
+
+  var buttons_con = document.querySelectorAll('.task-card__button.task-card__button_stretch.btn.btn-bordered');
+  buttons_con.forEach(function(button) {
+    button.click();
+  });
+}
 })();
